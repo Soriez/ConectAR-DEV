a2067ce7509de1b02e9f3df6014ff307b6aa60e4
predondo29 <prisredondo29@gmail.com>
2025-10-01 18:21:57 -0300

    Página de Log-In responsive

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,29 @@
-const path = require('path')
+import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-export default {
-    root: path.resolve(__dirname, 'src'),
+// Define __dirname (necesario cuando se usa ESM para obtener la ruta del directorio actual)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+    // La propiedad 'root' debe ser una función en Vite 5 para que 'path.resolve' funcione correctamente.
+    // Aunque en este caso, la configuración dentro de 'defineConfig' ya maneja la ruta base.
+    
+    
     server: {
         port: 8080,
         hot: true
     },
+    
     resolve: {
-    alias: {
-        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        alias: {
+            // Se usa '__dirname' para resolver la ruta a node_modules
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         }
+    },
+    
+    css: {
+        devSourcemap: true
     }
-}
+});

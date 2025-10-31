10ff9ebed4dc045fe1872cd76a3e1bea10cc5573
predondo29 <233138026+predondo29@users.noreply.github.com>
2025-10-31 18:28:29 -0300

    NavBar responsivo para toda la página

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import NavBar from "./components/NavBar/NavBar"
+
+
+const App = () => {
+    return (
+        <>
+            <header>
+                <NavBar />
+            </header>
+
+            <main className="container">
+
+
+            </main>
+
+            <footer>
+
+            </footer>
+        </>
+    )
+}
+
+export default App

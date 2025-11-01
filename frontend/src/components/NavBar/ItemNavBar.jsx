a5927ffc05c6ef6ca6b0bb13d245c67c29826aa7
Santiago Oriez <oriezsantiagogabriel@gmail.com>
2025-11-01 14:32:22 -0300

    agrego funcion para el scrol en seccion servicios

--- frontend/src/components/NavBar/ItemNavBar.jsx
+++ frontend/src/components/NavBar/ItemNavBar.jsx
@@ -2,10 +2,31 @@ import { NavLink } from "react-router"
 
 
 const ItemNavBar = ( { link, isMobile, setOpen } ) => {
-
+    // Clases CSS comunes para todos los enlaces
     const commonClasses = "text-white hover:text-blue-400 transition-colors duration-150 ease-in-out cursor-pointer";
 
-    const handleClick = () => {
+      
+     //Función que maneja el click en cada enlace del navbar
+     //@param {Event} e - Evento del click
+     
+     //cambios para agregar scrol hasta las secciones 
+    const handleClick = (e) => {
+        // Verificamos si el link tiene la propiedad scrollTo
+        // Si la tiene, significa que queremos hacer scroll en lugar de navegar
+        if(link.scrollTo){
+            e.preventDefault();// Prevenimos la navegación por defecto de React Router
+             // Buscamos el elemento HTML con el ID especificado en scrollTo
+             const elemento = document.getElementById(link.scrollTo)
+             //si encontramos el elemento, hacemos el scroll suave hacia él
+             if(elemento){
+                elemento.scrollIntoView({
+                    behavior: 'smooth',// Animación suave del scroll
+                    block: 'start' // El elemento se posiciona al inicio del viewport
+                    }
+                )
+            }
+        }
+        // Si estamos en mobile y el menú está abierto, lo cerramos después del click
         if (isMobile && setOpen) {
             setOpen(false);
         }
@@ -17,7 +38,7 @@ const ItemNavBar = ( { link, isMobile, setOpen } ) => {
         <NavLink
             to={link.href}
             className={`${commonClasses} ${isMobile ? "block text-lg " : "text-sm  px-3 py-2 rounded-md"}`}
-            onClick={ handleClick }
+            onClick={ handleClick }//Agregamos el manejador de click personalizado
         >
             {link.name}
         </NavLink>

63caf965912393c2832588a537927a208642af9d
predondo29 <233138026+predondo29@users.noreply.github.com>
2025-11-18 19:55:56 -0300

    .

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -18,19 +18,6 @@ const Inicio = () => {
         <ContactoInicio />
       </div>
     </div>
-
-  // Opcion 2: fondos separados con un gradiente intermedio
-    // <div>
-    //   <div className='bg-blue-950'>
-    //     <HeaderInicio />
-    //   </div>
-    //   <div className="h-20 bg-gradient-to-b from-blue-950 to-blue-800"/>
-    //   <div className=' from-blue-800 to-blue-200 bg-linear-to-b'>
-    //     <ServiciosInicio />
-    //     <FreelancersInicio />
-    //     <ContactoInicio />
-    //   </div>
-    // </div>
   )
 }
 
--- frontend/src/pages/RecuperarCuenta.jsx
+++ frontend/src/pages/RecuperarCuenta.jsx
@@ -132,7 +132,7 @@ const RecuperarCuenta = () => {
                             
                             {/* Botón de recuperar (con tu estilo original) */}
                             <button type="submit" 
-                                className="w-full bg-gradient-to-br from-blue-600 to-blue-800 border-0 text-white px-6 py-3.5 rounded-lg text-base font-semibold transition-all duration-300 shadow-lg shadow-blue-600/30 cursor-pointer hover:from-blue-700 hover:to-blue-900 order-1 md:order-2"
+                                className="w-full bg-linear-to from-blue-600 to-blue-800 border-0 text-white px-6 py-3.5 rounded-lg text-base font-semibold transition-all duration-300 shadow-lg shadow-blue-600/30 cursor-pointer hover:from-blue-700 hover:to-blue-900 order-1 md:order-2"
                             >
                                 Recuperar Cuenta
                             </button>
